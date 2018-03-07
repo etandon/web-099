2fc57ead8f54d66be7d9891ce7f816be116d122d
eshantandon <eshantandon@gmail.com>
2018-03-06 22:01:50 -0500

    Shell

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * Created by eshan.tandon on 3/4/18.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+
+    }
+}
---
+++ src/Solution1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by eshan.tandon on 3/4/18.
+ */
+public class Solution1 {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+    }
+}
